80339396402b20c3aabc9c4fbef89d2d588f0a76
lilia00kh <lilia00kh@gmail.com>
2020-04-16 11:37:37 +0300

    commit

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const pug = require('pug');
 const path = require('path');
-
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'static', 'views'));
+app.set('views', path.join(__dirname, 'static', 'view'));
+
+app.get('/',(req,res)=>{
+    res.render("layout")
+})
+
 
 app.listen(3001, () => {
     console.log(3001)
